726296d4737509e29437ce64982e3a6385d521c0
Rony-star666 <1216005673@qq.com>
2025-10-19 17:47:50 +1000

    implement minimal transforms
    implement minimal transforms

--- recognition/dataset.py
+++ recognition/dataset.py
@@ -10,7 +10,14 @@ def set_seed(seed: int = 42):
     torch.cuda.manual_seed_all(seed)
 
 def get_transforms(img_size=224, gray=True, aug=True):
-    raise NotImplementedError("transforms not implemented yet")
+    t = [transforms.Resize((img_size, img_size)), transforms.ToTensor()]
+    if gray:
+        t.append(transforms.Normalize(mean=[0.5], std=[0.5]))
+        return transforms.Compose([transforms.Grayscale(num_output_channels=1)] + t)
+    else:
+        t.append(transforms.Normalize(mean=[0.485,0.456,0.406],
+                                      std=[0.229,0.224,0.225]))
+        return transforms.Compose(t)
 
 def get_loaders(data_root="ADNI/AD_NC", img_size=224, batch_size=32, num_workers=4, gray=True):
     raise NotImplementedError("loaders not implemented yet")
