71b0133b62982fd0291207fe254701b17698a669
Rony-star666 <1216005673@qq.com>
2025-10-20 15:18:37 +1000

    Implement model loading from checkpoint using build_model()

--- recognition/GFNet_Alzheimer_49088847/predict.py
+++ recognition/GFNet_Alzheimer_49088847/predict.py
@@ -1,6 +1,18 @@
 import torch
 import argparse
+from torchvision import transforms
 
+def build_transform(img_size=224, gray=True):
+    t = [transforms.Resize((img_size, img_size))]
+    if gray:
+        t = [transforms.Grayscale(num_output_channels=1)] + t
+    t += [transforms.ToTensor()]
+    if gray:
+        t += [transforms.Normalize([0.5],[0.5])]
+    else:
+        t += [transforms.Normalize([0.485,0.456,0.406],[0.229,0.224,0.225])]
+    return transforms.Compose(t)
+    
 def main(args):
     print("Prediction script initialized")
 
